9ad75f998cdc3a1e7d2c8119a305c17c831b477b
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-08-15 21:50:12 -0700

    tests and endpoints for put and delete written

--- server.js
+++ server.js
@@ -26,8 +26,31 @@ app.post('/items', function(req, res) {
 			return res.status(500).json({ message: 'Internal Server Error'});
 		}
 
-		console.log('Item created');
-		return res.status(201).json(item);
+		return res.status(201).json(item.name);
+	});
+});
+
+app.put('/items/:id', function(req, res) {
+
+	Item.findOneAndUpdate({ _id: req.params.id }, { name: req.body.name }, function(err, item) {
+		
+		// TODO -- figure out validation thorugh Mongoose
+		if(err || !req.body.name) {
+			return res.status(500).json({ message: 'Internal Server Error' });
+		}
+
+		return res.status(200).json(item);
+	});
+});
+
+app.delete('/items/:id', function(req, res) {
+
+	Item.findOneAndRemove({ _id: req.params.id }, function(err, item) {
+		if(err) {
+			return res.status(500).json({ message: 'Internal Server Error' });
+		}
+
+		return res.status(200).json(item);
 	});
 });
 
--- test/server-test.js
+++ test/server-test.js
@@ -70,8 +70,8 @@ describe('POST request on /items', function() {
 				should.equal(err, null);
 				res.should.have.status(201);
 				res.should.be.json;
-				res.body.should.be.a('object');
-				res.body.name.should.equal('salad');
+				res.body.should.be.a('string');
+				res.body.should.equal('salad');
 				done();
 			});
 	});
@@ -99,7 +99,7 @@ describe('POST request on /items', function() {
 		return postError('/items', request, 500, done);
 	}
 
-	it('no JSON in body should return 500 INTERNAL SERVER ERROR', function(done) {
+	it('no JSON in request should return 500 INTERNAL SERVER ERROR', function(done) {
 		serverError('string', done);
 	});
 
@@ -111,9 +111,98 @@ describe('POST request on /items', function() {
 		serverError({ name: [] }, done);
 	});
 
+	removeSampleItems();
+});
 
+function printItems(done) {
+	chai.request(app)
+		.get('/items')
+		.end(function(err, res) {
+			console.log(res.body);
+			done();
+		});
+}
 
+describe('PUT request on /items/:id', function() {
+	createSampleItems();
+	var validEndpoint = '/items/57b17fbcd78c07ceb69ecb64';
 
+	it('valid request should edit the item and return 200', function(done) {
+		chai.request(app)
+			.put(validEndpoint)
+			.send({ name: 'hot dogs' })
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.should.be.json;
+				done();
+			});
+	});
+
+	it('invalid endpoint should return 404 NOT FOUND', function(done) {
+		putError('/aaaa/bbb', { name: 'chicken wings' }, 404, done);
+	});
+
+	function putError(endpoint, request, error, done) {
+		chai.request(app)
+			.put(endpoint)
+			.send(request)
+			.end(function(err, res) {
+				res.should.have.status(error);
+				done();
+			});
+	}
+
+	it('request to change item not in database should return 500 INTERNSAL SERVER ERROR', function(done) {
+		serverError('/items/abcdefg', { name: 'beef' }, done);
+	});
+
+	function serverError(endpoint, request, done) {
+		return putError(endpoint, request, 500, done);
+	}
+
+	// Having trouble making this one work... not sure what it is.
+	it('request with nothing in body should return 500 INTERNAL SERVER ERROR', function(done) {
+		serverError(validEndpoint, '', done);
+	});
+
+	it('request with invalid JSON in body should return 500 INTERNAL SERVER ERROR', function(done) {
+		serverError(validEndpoint, 'string', done);
+	});
+
+	it('request without "name" property should return 500 INTERNAL SERVER ERROR', function(done) {
+		serverError(validEndpoint, { price: 'twelve' }, done);
+	});
+
+	// TODO -- figure out Mongoose validation
+	// it('request without a string in "name" property should return 500 INTERNAL SERVER ERROR', function(done) {
+	// 	serverError(validEndpoint, { name: [] }, done);
+	// });
+
+	removeSampleItems();
+});
+
+describe('DELETE request on /items/:id', function() {
+	createSampleItems();
+	var validEndpoint = '/items/57b17fbcd78c07ceb69ecb64';
+
+	it('valid request should return 200 and deleted item', function(done) {
+		chai.request(app)
+			.delete(validEndpoint)
+			.end(function(err, res) {
+				should.equal(err, null);
+				res.should.have.status(200);
+				done();
+			});
+	});
+
+	it('request to delete nonexistent item should return 500 INTERNAL SERVER ERROR', function(done) {
+		chai.request(app)
+			.delete('/items/abcdefg')
+			.end(function(err, res) {
+				res.should.have.status(500);
+				done();
+			});
+	});
 
 	removeSampleItems();
 });
