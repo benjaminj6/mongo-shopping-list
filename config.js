f4a13231a64c1c9aa7ae8869a24f743fdd3cf9bf
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-08-15 22:29:16 -0700

    set mongo to mlab

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL||
 											 global.DATABASE_URL||
 											 (process.env.NODE_ENV === 'production' ? 
 											 		'mongodb://localhost/shopping-list' :
-											 		'mongodb://localhost/shopping-list-dev');
+											 		'mongodb://benjamin:asdfghjkl;\'@ds147905.mlab.com:47905/shopping-list');
 
 exports.PORT = process.env.PORT || 8080;	
